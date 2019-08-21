b0d61bd8996a34daa67da0a6d2e31fdc653929cb
Jan Hadáček <janh@blindshell.com>
2019-08-21 17:36:51 +0200

    Simple list flightday airplanes.

--- soarspy/glidernet_db.py
+++ soarspy/glidernet_db.py
@@ -1,13 +1,34 @@
-import datetime
+from datetime import date,datetime
+from typing import Sequence
+import os.path
 import sqlite3
+import glob
+from dateutil import tz
 
-def load_flight_day(date):
-    pass
 
 class GlidernetDb():
+    def __init__(self, db):
+        self.db = db
 
-    def __init__(self):
-        pass
+    def get_airplanes(self) -> Sequence[sqlite3.Row]:
+        c = self.db.execute("select address, max(altitude) as max_qnh, "
+                            "min(reference_timestamp) as 'first_seen [epoch]', "
+                            "max(reference_timestamp) as 'last_seen [epoch]'"
+                            "from positions group by address order by min(reference_timestamp) asc")
+        return c.fetchall()
 
+
+def list_flight_days() -> Sequence[str]:
+    day_files = glob.glob("data/gliderdata-*.db")
+    return list(map(lambda d: os.path.basename(d).replace('gliderdata-', '').replace('.db',''), day_files))
+
+
+def load_flight_day(daystr: str) -> GlidernetDb:
+    filename = "data/gliderdata-"+daystr+".db"
+    if os.path.exists(filename):
+        db = sqlite3.connect(filename, detect_types=sqlite3.PARSE_COLNAMES)
+        sqlite3.register_converter('epoch', lambda a: datetime.fromtimestamp(int(a), tz.tzlocal()))
+        db.row_factory = sqlite3.Row
+        return GlidernetDb(db)
 
 
--- soarspy/views.py
+++ soarspy/views.py
@@ -1,11 +1,23 @@
-from soarspy import app
-from flask import render_template, redirect
+import re
+
+from soarspy import app, glidernet_db
+from flask import render_template, redirect, request
+
 
 @app.route('/')
 def index():
-    return redirect("/airplanes")
+    return redirect("/flight-days")
 
 
-@app.route('/airplanes')
+@app.route('/flight-days')
 def airplanes():
-    return render_template("airplanes.html")
+    template_vars = {"flight_days": glidernet_db.list_flight_days()}
+
+    date = request.args.get('date')
+
+    if date is not None and re.match('^(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)$', date) is not None:
+        db = glidernet_db.load_flight_day(date)
+        template_vars['airplanes'] = db.get_airplanes()
+        print(type(template_vars['airplanes'][0]['last_seen'].tzinfo))
+
+    return render_template("flight_days.html", **template_vars)
